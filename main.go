1f0872358edaa43c09ce8575ee06af87fbfba94b
@bellamariz <bellamariz@aluno.puc-rio.br>
2023-08-09 11:40:22 -0300

    Organize output

--- main.go
+++ main.go
@@ -1,22 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"fullcycle-golang-course/main/foundation"
 )
 
+func separator(s string) {
+	fmt.Printf("\n-------------------------------------------------\n")
+	fmt.Printf("--------------------%s--------------------\n", s)
+	fmt.Printf("-------------------------------------------------\n")
+}
+
 func callFoundation() {
+	separator("Basics")
 	foundation.Types()
 	foundation.Arrays()
 	foundation.Slices()
 	foundation.Maps()
 	foundation.Functions()
+
+	separator("Structs")
 	foundation.Structs()
 	foundation.Pointers()
+
+	separator("Data Types")
 	foundation.TypeAssertion()
 	foundation.Generics()
 }
 
 func main() {
-	// callFoundation()
-	// foundation.Generics()
+	callFoundation()
 }
