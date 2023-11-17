b9da25308d57f9faf8ec504173d649505dc02b99
@bellamariz <isabellafmariz@gmail.com>
2023-11-17 15:42:47 -0300

    feat: Start new course section (Notable Packages)

---
+++ notable_packages/notable_packages.go
@@ -0,0 +1,16 @@
+// FullCycle: Go Expert - Part 2 (Notable Packages)
+package notable_packages
+
+import (
+	"fmt"
+)
+
+func separator(s string) {
+	fmt.Printf("\n#################################################")
+	fmt.Printf("\n##### %s #####", s)
+	fmt.Printf("\n#################################################\n\n")
+}
+
+func CallNotablePackages() {
+	separator("Manipulating Files")
+}
